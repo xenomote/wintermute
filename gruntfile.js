feff328912f6639a9fa5cfbb500f4b1f41a3869a
alexander williamson <xenomote@gmail.com>
2023-07-10 01:00:17 +0100

    use grunt options

--- gruntfile.js
+++ gruntfile.js
@@ -1,12 +1,18 @@
 module.exports = function(grunt) {
+    let email = grunt.option('email')
+    let token = grunt.option('token')
+
+    if (!email || !token) {
+        grunt.fail.fatal('email and token options must be set!')
+    }
 
     grunt.loadNpmTasks('grunt-screeps');
 
     grunt.initConfig({
         screeps: {
             options: {
-                email: process.env.SCREEPS_EMAIL,
-                token: process.env.SCREEPS_TOKEN,
+                email,
+                token,
                 branch: 'default',
             },
             dist: {
